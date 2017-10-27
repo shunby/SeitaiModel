9ce22202300eeb223345a9f2af36a89034b2c637
shunby <taby@outlook.jp>
2017-10-27 15:01:57 +0900

    大気の物質の量を修正

--- SeitaiModel/src/seitaimodel/node/Air.java
+++ SeitaiModel/src/seitaimodel/node/Air.java
@@ -9,15 +9,17 @@ public class Air extends Node {
 	public Air(Pane pane, double x, double y) {
 		super(pane, "Air", x, y, 260, 130);
 		/*
-		 * 大気圧: 1013hPa = 10130000N/m^2
+		 * 大気圧: 1013hPa = 101300N/m^2
+		 * g = 10とする
+		 * 大気の重さ(kg) = 10130kg;
 		 * 面積を1m^2とする
-		 * 10130000 * 0.78 = 7900000:N2
-		 * 10130000 * 0.21 = 2100000:O2
-		 * 10130000 * 0.00032 = 3200:CO2
+		 * 10130 * 0.78 = 7900:N2
+		 * 10130 * 0.21 = 2100:O2
+		 * 10130 * 0.00032 = 3.2:CO2
 		 */
-		nitrogen= new BigDecimal("7900000");
-		oxygen	= new BigDecimal("2100000");
-		carbon	= new BigDecimal("3200");
+		nitrogen= new BigDecimal("7900");
+		oxygen	= new BigDecimal("2100");
+		carbon	= new BigDecimal("3.2");
 
 	}
 
